c99de9deb461148d27c6fa6cdeecd6b3e12fbdb8
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-05-19 13:44:15 -0400

    action prompt and switch case

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,8 +1,8 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var cTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -13,3 +13,30 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
 });
+
+function start() {
+  inquirer.prompt({
+    type: "list",
+    name:"action",
+    message: "What would you like to do?",
+    choices: [
+      "Add employees, departments, and roles",
+      "View emplyees, departments, and roles",
+      "Update employee roles"
+    ]
+  }).then(function(answer) {
+    switch (answer.action) {
+      case "Add employees, departments, and roles":
+        // some function
+        break;
+
+      case "View emplyees, departments, and roles":
+        // some function
+        break;
+
+      case "Update employee roles":
+        // some function
+        break;
+    }
+  });
+}
