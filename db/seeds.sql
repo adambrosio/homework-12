c07a57f152576cc4760b1cc2ac44dcd01f051553
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-05-27 17:43:13 -0400

    functioning functions...

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,14 +5,21 @@ INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO department (name) VALUES ("Legal");
 INSERT INTO department (name) VALUES ("Marketing");
 
-INSERT INTO role (title, salary, department_id) VALUES ("IT Manager", 100000, 1), ("Lead Engineer", 100000, 1), ("Junior Developer", 70000, 1), ("Salesperson", 60000, 2), ("Sales Manager", 70000, 2), ("Lawyer", 100000, 3), ("Lead Marketer", 85000, 4), ("Junior Marketer", 70000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("IT Manager", 100000, 1), ("Lead Engineer", 100000, 1), ("Software Engineer", 80000, 1), ("Salesperson", 60000, 2), ("Sales Manager", 70000, 2), ("Lawyer", 100000, 3), ("Lead Marketer", 85000, 4), ("Junior Marketer", 70000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Bobby", "D", 1, null),
-("Ben", "Dover", 2, 1),
-("Stan", "Dup", 3, 1);
+VALUES ("Michael", "Corleone", 1, null),
+("Tony", "Soprano", 2, 1),
+("Tony", "Montana", 3, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tony", "Goombah", 5, null),
-("Lenny", "Lanes", 4, 4),
+VALUES ("Sam", "Rothstein", 5, null),
+("Henry", "Hill", 4, 4),
 ("Jerry", "Colombo", 4, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Hagen", 6, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Luca", "Changretta", 7, null),
+("Luca", "Brasi", 8, 8);
--- employeeTracker.js
+++ employeeTracker.js
@@ -48,7 +48,7 @@ function start() {
         addEmployee();
         break;
 
-      case "Add Deparment":
+      case "Add Department":
         addDepartment();
         break;
 
@@ -62,33 +62,32 @@ function start() {
 
     }
   });
-
-  console.log("\n------------------------")
 }
-// Various functions to perform task depending on user selection from original prompt
-function viewEmployees() { 
-  var query = "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department ON role.department_id = department.id ORDER BY department.name DESC"; 
-  connection.query(query, function(err, res) {
+
+// Functions to perform task depending on user selection from original prompt
+function viewEmployees() {
+  var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department ON role.department_id = department.id ORDER BY employee.last_name ASC";
+  connection.query(query, function (err, res) {
     if (err) throw err;
-    console.table('\n',res);
+    console.table('\n', res);
   })
   start();
 }
 
 function viewDepartment() {
   var query = "SELECT * FROM department";
-  connection.query(query, function(err, res) {
+  connection.query(query, function (err, res) {
     if (err) throw err;
-    console.table(res);
+    console.table('\n', res);
   })
   start();
 };
 
-function viewRole(){
+function viewRole() {
   var query = "SELECT * FROM role";
-  connection.query(query, function(err, res) {
+  connection.query(query, function (err, res) {
     if (err) throw err;
-    console.table(res);
+    console.table('\n', res);
   })
   start();
 };
@@ -106,28 +105,54 @@ function addEmployee() {
       message: "Please enter employee's last name:"
     }
   ]).then(function ({ first_name, last_name }) {
-    connection.query("INSERT INTO employee (first_name, last_name) VALUES ?", (["first_name", "last_name"]), function(err, res){
+    connection.query("INSERT INTO employee (first_name, last_name) VALUES ?", (["first_name", "last_name"]), function (err, res) {
       if (err) throw err;
       console.log("Employee Added!");
-      console.log(res);
       console.table(res);
     });
-      start();
+    start();
   })
 }
 
 function addDepartment() {
-
+  inquirer.prompt({
+    type: "input",
+    name: "department",
+    message: "Please enter the department you would like to add:"
+  }).then(function(answer) {
+    connection.query("INSERT INTO department SET ?",
+    {
+      name: answer.department
+    },
+    function(err){
+      if (err) throw err;
+      console.log("The department was added successfully!");
+      start();
+    });
+    
+  })
 }
 
-function addRole(){
+function addRole() {
 
 }
 
 function updateRole() {
-
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "Please enter first name of employee whose role you wish to update:"
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "Please enter last name of employee whose role you wish to update:"
+    }
+  ]).then(function({ first_name, last_name }) {
+    var query = "UPDATE employee SET role_id"
+    connection.query()
+  })
 }
-
-
 
 start();
